20f0008d117bf881fdfba671cb2a5d1522d961c7
Matwi Ziad (Szf_N_2024_2026) <matwi.ziad@diak.szamalk-szalezi.hu>
2025-04-10 11:25:16 +0200

    Webshop kesz

--- Termek.js
+++ Termek.js
@@ -4,10 +4,23 @@ export default class Termek{
     constructor(termekObj,szElem){
         this.#termekObj = termekObj;
         this.szElem = szElem;
-        
+        this.megjelenit();
+        this.gombElem = document.querySelector(".termek:last-child");
+
+
     }
 
-
+    megjelenit(){
+        let html = `<div class="card shadow-sm">
+                    <img src="${this.#termekObj.kep}" class="card-img-top" alt="Product Image">
+                    <div class="card-body">
+                        <h5 class="card-title">${this.#termekObj.nev}</h5>
+                        <p class="card-text">Ár: ${this.#termekObj.ar}</p>
+                        <p class="card-text">Platform: ${this.#termekObj.platform}</p>
+                        <button class="btn btn-primary">Add to Cart</button>
+                    </div>
+                    </div>`
+    }
 
 
 }
