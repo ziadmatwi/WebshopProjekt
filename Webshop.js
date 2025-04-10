2e05cf9fa32ed6b36b9b040f9a5e7d2669e108f9
Matwi Ziad (Szf_N_2024_2026) <matwi.ziad@diak.szamalk-szalezi.hu>
2025-04-10 10:57:44 +0200

    Webshop class elkezdve

---
+++ Webshop.js
@@ -0,0 +1,19 @@
+import Termek from "./termekLista.js";
+import termekLista from "./termekLista.js"
+
+export default class Webshop{
+    
+
+
+    constructor(szElem){
+        this.szElem = szElem;
+        
+    }
+
+
+
+
+
+
+
+}
